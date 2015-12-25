7d4bba8a1224961e07d422c401c364a6d70e5c45
Colin Meinke <hello@colinmeinke.com>
2015-12-24 18:27:21 -0600

    Get Link url from ownProps

--- src/Link.js
+++ src/Link.js
@@ -6,11 +6,9 @@ const Link = ({ children, url, ...props }) => {
   return <a href={ url } { ...props }>{ children }</a>;
 };
 
-const ConnectedLink = connect(() => ({}), dispatch => ({
+const ConnectedLink = connect(() => ({}), ( dispatch, { url } ) => ({
   onClick: e => {
     e.preventDefault();
-    const { hash, pathname, search } = e.target;
-    const url = pathname + search + hash;
     window.history.pushState({}, '', url );
     dispatch( updateUrl( url ));
   },
