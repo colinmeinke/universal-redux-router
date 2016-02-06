0a1d56cebfe3b4461f37ef82fbdc2e79c3d14497
Colin Meinke <hello@colinmeinke.com>
2016-02-05 20:35:51 -0600

    test(actions): add update url tests

---
+++ test/actions/updateUrl.js
@@ -0,0 +1,19 @@
+import expect from 'expect';
+
+import updateUrl from '../../src/actions/updateUrl';
+import { UPDATE_URL } from '../../src/constants';
+
+describe( 'action creator', () => {
+  describe( 'updateUrl()', () => {
+    it( 'should create an action to update url', () => {
+      const url = '/hello-world';
+
+      const expectedAction = {
+        type: UPDATE_URL,
+        url,
+      };
+
+      expect( updateUrl( url )).toEqual( expectedAction );
+    });
+  });
+});
