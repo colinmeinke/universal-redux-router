e7453a25e700d728700593eddf1eaff166961b63
Colin Meinke <hello@colinmeinke.com>
2016-02-06 13:46:52 -0600

    test: fix change page to test

--- test/actions/changePageTo.js
+++ test/actions/changePageTo.js
@@ -6,7 +6,11 @@ import { CHANGE_PAGE_TO } from '../../src/constants';
 describe( 'action creator', () => {
   describe( 'changePageTo()', () => {
     it( 'should create an action to change page', () => {
-      const options = { shouldAddToHistory: false };
+      const options = {
+        shouldAddToHistory: false,
+        shouldScrollToTop: false,
+      };
+
       const to = '/hello-world';
 
       const expectedAction = {
