b8a7e3aefdfe157208b7e977f26d615f8a5ddc8f
Colin Meinke <hello@colinmeinke.com>
2016-02-09 13:08:48 -0600

    feat(reducer): allow actions action creator callbacks

--- src/reducers/router.js
+++ src/reducers/router.js
@@ -6,12 +6,38 @@ import { CHANGE_PAGE_TO } from '../constants';
 const router = reducers => {
   const rootReducer = combineReducers({ ...reducers, url });
 
+  const getNewStateFromAction = ( state, action ) => {
+    let newState = rootReducer( state, action );
+
+    if ( action.then ) {
+      newState = getNewStateFromActionCreator( newState, action.then );
+    }
+
+    return newState;
+  };
+
+  const getNewStateFromActionCreator = ( state, actionCreator ) => {
+    const action = actionCreator( state );
+
+    if ( action ) {
+      return getNewStateFromAction( state, action );
+    }
+
+    return state;
+  };
+
   const batchReducer = ( state, action ) => {
     switch ( action.type ) {
       case CHANGE_PAGE_TO:
-        return action.actions.reduce( rootReducer, state );
+        let newState = { ...state };
+
+        action.actions.forEach( a => {
+          newState = getNewStateFromAction( newState, a );
+        });
+
+        return newState;
       default:
-        return rootReducer( state, action );
+        return getNewStateFromAction( state, action );
     }
   };
 
